be164e3c79fc7ae0d2c8a71c9d7da9aa2838c1ce
Julian Picht <julian.picht@redsift.io>
2025-03-31 13:35:02 +0200

    dcfg: fix nil-map

--- dcfg/impl_registry.go
+++ dcfg/impl_registry.go
@@ -11,5 +11,8 @@ func Register(scheme string, factory func(*url.URL) (Backend, error)) {
 	if existing {
 		panic("two dcfg.Backend implementation for scheme \"+scheme+\"")
 	}
+	if impls == nil {
+		impls = make(map[string]func(*url.URL) (Backend, error))
+	}
 	impls[scheme] = factory
 }
