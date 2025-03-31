7e845b16399b75dfa78a7a4379cc02009010f29d
Julian Picht <julian.picht@redsift.io>
2025-03-31 11:14:47 +0200

    platform: add blocked accounts

---
+++ platform/blocked_accounts.go
@@ -0,0 +1,26 @@
+package platform
+
+import (
+	"context"
+
+	"github.com/redsift/go-cfg/dcfg"
+	"github.com/redsift/go-siftjson"
+)
+
+const KEY_ACCOUNTS = "accounts"
+
+var BlockedAccountssV1Key = SliceKey[BlockedAccount](1, KEY_ACCOUNTS, KEY_BLOCKED)
+
+type BlockedAccount struct {
+	Account siftjson.AccountID
+	Reason  string
+}
+
+func BlockedAccounts(b dcfg.Backend) *dcfg.TypedSlice[BlockedAccount] {
+	res, _ := dcfg.NewTypedSlice[BlockedAccount](b, BlockedSiftsV1Key)
+	return res
+}
+
+func LoadBlockedAccounts(ctx context.Context, b dcfg.Backend) (out []BlockedAccount, _ error) {
+	return BlockedAccounts(b).Load(ctx)
+}
